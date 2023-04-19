afa9816ab44761ea55b2588e97a4d1728da84675
Mariam-Fathi <66276072+Mariam-Fathi@users.noreply.github.com>
2023-04-19 21:43:08 +0200

    Update ag.java

--- ag.java
+++ ag.java
@@ -1,18 +1,53 @@
-public class ag {
-    public static void main(String[] args) {
+public class output {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("hello world");
         boolean a = true;
         boolean b = false;
 
+        int x = 10;
+        int y = 5;
+        int z = 2;
+
+        
         if (a) {
             System.out.println("a is true");
-        } else {
-            System.out.println("a is false");
         }
+
 
         if (b) {
             System.out.println("b is true");
         } else {
             System.out.println("b is false");
         }
+        for(int i = 0 ; i < 3  ;i++)
+        {
+            System.out.println("hello");
+        }
+
+       
+        if (x > y) {
+            if (x > z) {
+                System.out.println("x is the largest number");
+            } else {
+                System.out.println("z is the largest number");
+            }
+        } else {
+            if (y > z) {
+                System.out.println("y is the largest number");
+            } else {
+                System.out.println("z is the largest number");
+            }
+        }
+        int var = 1; // initialize the loop counter
+
+        while ( var <= 5) { // specify the condition
+            System.out.println(var); // code to be executed
+            var++; // increment the loop counter
+        }
+
     }
+
+
+
 }
